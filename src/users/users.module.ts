a47d0a00c9730edbe57123fd478935d4b2f2ff3f
keleris32 <charles.ejiegbu@gmail.com>
2022-07-31 01:21:35 +0100

    feat: added repo, services & controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { UsersService } from './users.services';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
 })
 export class UserModule {}
