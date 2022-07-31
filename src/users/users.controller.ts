6ae62b66963ee4020034bd85aef744ff5c892667
keleris32 <charles.ejiegbu@gmail.com>
2022-07-31 01:20:59 +0100

    feat: added controllers

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+import { User } from './schemas/user.schema';
+import { UsersService } from './users.services';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':userId')
+  async getUserById(@Param('userId') userId: string): Promise<User> {
+    return this.usersService.getUserById(userId);
+  }
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(createUserDto.email, createUserDto.age);
+  }
+
+  @Patch(':userId')
+  async updateUser(
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateUser(userId, updateUserDto);
+  }
+}
